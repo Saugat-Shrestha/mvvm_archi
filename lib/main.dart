42aa6f083316aa6bb0adb475c84f4f2b075a5fa3
Saugat-Shrestha <shresthaasaugat31@gmail.com>
2025-05-23 22:33:17 +0545

    Add: many components

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_architecture/app/app.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
+ 
